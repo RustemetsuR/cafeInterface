60e284dc08c9dadeca883a9bb3b4083754e97b25
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-09-21 02:28:54 +0600

    Added all missing semi colons

--- src/store/reducers/menuReducer.js
+++ src/store/reducers/menuReducer.js
@@ -12,36 +12,36 @@ const initialState = {
 const menuReducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_MENU_REQUEST:
-            return { ...state, loading: true }
+            return { ...state, loading: true };
         case FETCH_MENU_SUCCESS:
-            return { ...state, loading: false, menu: action.menu }
+            return { ...state, loading: false, menu: action.menu };
         case FETCH_MENU_FAILURE:
-            return { ...state, loading: false, error: action.error }
+            return { ...state, loading: false, error: action.error };
         case ADD_TO_CART:
             let addedFood = state.menu.find(food => food.id === action.id);
             let alreadyExistedFood = state.cart.find(food => action.id === food.id);
             if (alreadyExistedFood) {
                 addedFood.quantity += 1;
                 addedFood.totalPrice = addedFood.price * addedFood.quantity;
-                return { ...state, totalPrice: state.totalPrice + addedFood.price }
+                return { ...state, totalPrice: state.totalPrice + addedFood.price };
             } else {
                 addedFood.quantity = 1;
                 addedFood.totalPrice = addedFood.price * addedFood.quantity;
                 let newTotal = state.totalPrice + addedFood.totalPrice;
-                return { ...state, cart: [...state.cart, addedFood], totalPrice: newTotal }
+                return { ...state, cart: [...state.cart, addedFood], totalPrice: newTotal };
             };
         case REMOVE_FROM_CART:
             let foodToRemove = state.cart.find(food => action.id === food.id);
             let newCart = state.cart.filter(food => action.id !== food.id);
             let newTotal = state.totalPrice - (foodToRemove.price * foodToRemove.quantity)
-            return { ...state, cart: newCart, totalPrice: newTotal }
+            return { ...state, cart: newCart, totalPrice: newTotal };
         case CLEAR:
-            return {...state, cart: [], totalPrice: 150, modal: false}
+            return {...state, cart: [], totalPrice: 150, modal: false};
         case SET_MODAL:
             return { ...state, modal: action.modal };
         default:
-            return state
-    }
-}
+            return state;
+    };
+};
 
 export default menuReducer;
