b384f7f9149be44e833c787e1bfe18065e8ddfa5
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-09-21 02:27:40 +0600

    Added all missing semi colons

--- src/store/actions/menuActions.js
+++ src/store/actions/menuActions.js
@@ -3,31 +3,31 @@ const { FETCH_MENU_SUCCESS, FETCH_MENU_REQUEST, FETCH_MENU_FAILURE, ADD_TO_CART,
 
 export const addToCart = id => {
     return { type: ADD_TO_CART, id };
-}
+};
 
 export const removeFromCart = id => {
     return { type: REMOVE_FROM_CART, id };
-}
+};
 
 export const setModal = modal => {
     return { type: SET_MODAL, modal };
-}
+};
 
 const fetchMenuSuccess = menu => {
-    return { type: FETCH_MENU_SUCCESS, menu }
-}
+    return { type: FETCH_MENU_SUCCESS, menu };
+};
 
 const fetchMenuRequest = () => {
-    return { type: FETCH_MENU_REQUEST }
-}
+    return { type: FETCH_MENU_REQUEST };
+};
 
 const fetchMenuFailure = error => {
-    return { type: FETCH_MENU_FAILURE, error }
-}
+    return { type: FETCH_MENU_FAILURE, error };
+};
 
 export const clear = () => {
     return { type: CLEAR };
-}
+};
 
 
 export const fetchMenuData = () => {
@@ -41,6 +41,6 @@ export const fetchMenuData = () => {
             dispatch(fetchMenuSuccess(fetchedMenu));
         } catch (e) {
             dispatch(fetchMenuFailure(e));
-        }
-    }
-}
+        };
+    };
+};
